cee46ec71f4ae2ec1399619fa2c2cda83368f02a
thanhhang31023 <thanhhang31023@gmail.com>
2025-03-05 21:47:26 +0900

    Add the 'Edit task' feature

--- main.js
+++ main.js
@@ -4,6 +4,22 @@ const taskList = document.querySelector("#task-list");
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
 
+taskList.onclick = function (e) {
+    const taskItem = e.target.closest(".task-item");
+    const taskIndex = +taskItem.getAttribute("task-index");
+    const task = tasks[taskIndex];
+
+    if (e.target.closest(".edit")) {
+        const newTitle = prompt("Enter the new task title:", task.title);
+        task.title = newTitle;
+        render();
+    } else if (e.target.closest(".done")) {
+        console.log("Mark as done/undone");
+    } else if (e.target.closest(".delete")) {
+        console.log("Delete");
+    }
+};
+
 todoForm.onsubmit = function (e) {
     e.preventDefault();
 
@@ -30,8 +46,10 @@ todoForm.onsubmit = function (e) {
 function render() {
     const html = tasks
         .map(
-            (task) => `
-    <li class="task-item ${task.completed ? "completed" : ""}">
+            (task, index) => `
+    <li class="task-item ${
+        task.completed ? "completed" : ""
+    }" task-index="${index}">
         <span class="task-title">${task.title}</span>
         <div class="task-action">
             <button class="task-btn edit">Edit</button>
